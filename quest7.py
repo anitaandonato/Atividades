9af8fd7a8afb86e10aa51c5217f7e512c64288d4
Anita <130478177+anitaandonato@users.noreply.github.com>
2024-10-10 22:51:54 -0300

    Create quest7.py

---
+++ quest7.py
@@ -0,0 +1,6 @@
+def afn_comeca_01_termina_10(string):
+    return string.startswith('01') and string.endswith('10')
+
+
+print(afn_comeca_01_termina_10("0110"))  
+print(afn_comeca_01_termina_10("0111"))  
